0ff1ba2bfb45291842a990ec3da30a677710fb17
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2021-09-01 18:06:46 +0700

    update list movie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -49,6 +49,172 @@ const Home = () => {
             name: 'Flim'
         }
     ])
+    const [dataMovie, setDataMovie] = useState([
+        {
+            id: 1,
+            name: 'Serial TV',
+            data: [
+                {
+                    id: 1,
+                    name: 'Laskar Pelangi',
+                    img: 'https://images.tokopedia.net/img/cache/700/product-1/2020/7/20/batch-upload/batch-upload_e2df3241-afc4-41e4-b1a3-b4263b2d05e9.jpeg'
+                },
+                {
+                    id: 2,
+                    name: 'Cek Toko Sebelah',
+                    img: 'https://s3.bukalapak.com/img/3805575796/large/index6.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Warkop DKI Reborn',
+                    img: 'https://awsimages.detik.net.id/community/media/visual/2016/08/02/05ec4ce6-6a37-49a2-9910-33722bd206a4.jpg?w=650&q=90'
+                },
+                {
+                    id: 4,
+                    name: 'Mariposa',
+                    img: 'https://assets.jalantikus.com/assets/cache/0/0/userfiles/2020/09/15/poster-film-indonesia-terbaru-55755.jpg'
+                },
+                {
+                    id: 5,
+                    name: 'Dilan 1990',
+                    img: 'https://s2.bukalapak.com/img/7057854092/large/poster_film_dilan.jpeg'
+                },
+                {
+                    id: 6,
+                    name: 'Bucin',
+                    img: 'https://pbs.twimg.com/media/ERsp6V1VUAAgkJp.jpg'
+                },
+                {
+                    id: 7,
+                    name: 'Hit & Run',
+                    img: 'https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/04/f4346585deae4a603386b3967ecdbdd6.jpg'
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Hiburan',
+            data: [
+                {
+                    id: 1,
+                    name: 'Laskar Pelangi',
+                    img: 'https://images.tokopedia.net/img/cache/700/product-1/2020/7/20/batch-upload/batch-upload_e2df3241-afc4-41e4-b1a3-b4263b2d05e9.jpeg'
+                },
+                {
+                    id: 2,
+                    name: 'Cek Toko Sebelah',
+                    img: 'https://s3.bukalapak.com/img/3805575796/large/index6.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Warkop DKI Reborn',
+                    img: 'https://awsimages.detik.net.id/community/media/visual/2016/08/02/05ec4ce6-6a37-49a2-9910-33722bd206a4.jpg?w=650&q=90'
+                },
+                {
+                    id: 4,
+                    name: 'Mariposa',
+                    img: 'https://assets.jalantikus.com/assets/cache/0/0/userfiles/2020/09/15/poster-film-indonesia-terbaru-55755.jpg'
+                },
+                {
+                    id: 5,
+                    name: 'Dilan 1990',
+                    img: 'https://s2.bukalapak.com/img/7057854092/large/poster_film_dilan.jpeg'
+                },
+                {
+                    id: 6,
+                    name: 'Bucin',
+                    img: 'https://pbs.twimg.com/media/ERsp6V1VUAAgkJp.jpg'
+                },
+                {
+                    id: 7,
+                    name: 'Hit & Run',
+                    img: 'https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/04/f4346585deae4a603386b3967ecdbdd6.jpg'
+                }
+            ]
+        },
+        {
+            id: 3,
+            name: 'Anime',
+            data: [
+                {
+                    id: 1,
+                    name: 'Laskar Pelangi',
+                    img: 'https://images.tokopedia.net/img/cache/700/product-1/2020/7/20/batch-upload/batch-upload_e2df3241-afc4-41e4-b1a3-b4263b2d05e9.jpeg'
+                },
+                {
+                    id: 2,
+                    name: 'Cek Toko Sebelah',
+                    img: 'https://s3.bukalapak.com/img/3805575796/large/index6.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Warkop DKI Reborn',
+                    img: 'https://awsimages.detik.net.id/community/media/visual/2016/08/02/05ec4ce6-6a37-49a2-9910-33722bd206a4.jpg?w=650&q=90'
+                },
+                {
+                    id: 4,
+                    name: 'Mariposa',
+                    img: 'https://assets.jalantikus.com/assets/cache/0/0/userfiles/2020/09/15/poster-film-indonesia-terbaru-55755.jpg'
+                },
+                {
+                    id: 5,
+                    name: 'Dilan 1990',
+                    img: 'https://s2.bukalapak.com/img/7057854092/large/poster_film_dilan.jpeg'
+                },
+                {
+                    id: 6,
+                    name: 'Bucin',
+                    img: 'https://pbs.twimg.com/media/ERsp6V1VUAAgkJp.jpg'
+                },
+                {
+                    id: 7,
+                    name: 'Hit & Run',
+                    img: 'https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/04/f4346585deae4a603386b3967ecdbdd6.jpg'
+                }
+            ]
+        },
+        {
+            id: 4,
+            name: 'Film',
+            data: [
+                {
+                    id: 1,
+                    name: 'Laskar Pelangi',
+                    img: 'https://images.tokopedia.net/img/cache/700/product-1/2020/7/20/batch-upload/batch-upload_e2df3241-afc4-41e4-b1a3-b4263b2d05e9.jpeg'
+                },
+                {
+                    id: 2,
+                    name: 'Cek Toko Sebelah',
+                    img: 'https://s3.bukalapak.com/img/3805575796/large/index6.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Warkop DKI Reborn',
+                    img: 'https://awsimages.detik.net.id/community/media/visual/2016/08/02/05ec4ce6-6a37-49a2-9910-33722bd206a4.jpg?w=650&q=90'
+                },
+                {
+                    id: 4,
+                    name: 'Mariposa',
+                    img: 'https://assets.jalantikus.com/assets/cache/0/0/userfiles/2020/09/15/poster-film-indonesia-terbaru-55755.jpg'
+                },
+                {
+                    id: 5,
+                    name: 'Dilan 1990',
+                    img: 'https://s2.bukalapak.com/img/7057854092/large/poster_film_dilan.jpeg'
+                },
+                {
+                    id: 6,
+                    name: 'Bucin',
+                    img: 'https://pbs.twimg.com/media/ERsp6V1VUAAgkJp.jpg'
+                },
+                {
+                    id: 7,
+                    name: 'Hit & Run',
+                    img: 'https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/04/f4346585deae4a603386b3967ecdbdd6.jpg'
+                }
+            ]
+        }
+    ])
     return (
         <div className="home">
             <Header />
@@ -65,6 +231,21 @@ const Home = () => {
                 { dataMenu.map((item) => 
                     <div className={item.name === 'Semua' ? "home-menu-item home-menu-item-active" : "home-menu-item"} key={item.id}>
                         <p className={item.name === 'Semua' ? "home-menu-item-text home-menu-item-text-active" : "home-menu-item-text"} >{item.name}</p>
+                    </div>
+                ) }
+            </div>
+            <div className="home-main">
+                { dataMovie.map((item) => 
+                    <div className="home-main-section" key={item.id}>
+                        <p className="home-main-section-title">{item.name}</p>
+                        <div className="home-main-section-content">
+                            { item.data.map((itemDetail) => 
+                                <div className="home-main-section-content-item" key={itemDetail.id}>
+                                    <img className="home-main-section-content-item-img" src={itemDetail.img} />
+                                    <p className="home-main-section-content-item-title">{itemDetail.name}</p>
+                                </div>
+                            ) }
+                        </div>
                     </div>
                 ) }
             </div>

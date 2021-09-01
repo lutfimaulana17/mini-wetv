65560b79aec9b7972927a5703db289f008ae0d9e
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2021-09-01 16:45:59 +0700

    update layout menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -27,17 +27,46 @@ const Home = () => {
             img: 'https://puui.qpic.cn/tv/0/1234997943_1680580/0'
         }
     ])
+    const [dataMenu, setDataMenu] = useState([
+        {
+            id: 1,
+            name: 'Semua'
+        },
+        {
+            id: 2,
+            name: 'Serial TV'
+        },
+        {
+            id: 3,
+            name: 'Hiburan'
+        },
+        {
+            id: 4,
+            name: 'Anime'
+        },
+        {
+            id: 5,
+            name: 'Flim'
+        }
+    ])
     return (
         <div className="home">
             <Header />
             <div className="home-wrapper-caurosel">
                 <Carousel autoPlay={true} interval={3000} showThumbs={false} showStatus={false}>
-                    { dataCaurosel.map((item, index) => 
-                        <div key={index}>
+                    { dataCaurosel.map((item) => 
+                        <div key={item.id}>
                             <img src={item.img} />
                         </div>
                     ) }
                 </Carousel>
+            </div>
+            <div className="home-menu">
+                { dataMenu.map((item) => 
+                    <div className={item.name === 'Semua' ? "home-menu-item home-menu-item-active" : "home-menu-item"} key={item.id}>
+                        <p className={item.name === 'Semua' ? "home-menu-item-text home-menu-item-text-active" : "home-menu-item-text"} >{item.name}</p>
+                    </div>
+                ) }
             </div>
         </div>
     )

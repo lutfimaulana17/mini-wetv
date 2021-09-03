fc15bd2a4e50ee2dcf5aa8485247b82123fe9523
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2021-09-03 14:05:13 +0700

    update testing

--- src/testing/Home.test.js
+++ src/testing/Home.test.js
@@ -4,19 +4,23 @@ import toJson from "enzyme-to-json";
 
 import { Home } from "../pages";
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/store';
 import { Footer, Header } from "../components";
-import { ShallowMock } from './shallow-mock.js';
 
 import { ImgLogo } from '../assets'
 
 describe("rendering components home", () => {
     it("renders Home component without crashing", () => {
-        shallow(ShallowMock(<Home />, store));
+        shallow(<Provider store={store}>
+                    <Router>
+                        <Home />
+                    </Router>
+                </Provider>);
     })
 
     it("renders Home header without crashing", () => {
-        const wrapper = shallow(<Header />)
+        const wrapper = mount(<Router><Header /></Router>)
 
         const headerLogo = (<img src={ImgLogo} className="header-img" />)
         expect(wrapper.contains(headerLogo)).toEqual(true);
@@ -26,35 +30,48 @@ describe("rendering components home", () => {
 
         const headerMenu2 = (<p className="header-menu">Semua</p>)
         expect(wrapper.contains(headerMenu2)).toEqual(true);
-
+        console.log(wrapper.find('#header-input-lang'))
         let defaultLang = wrapper.find('#header-input-lang').props().defaultValue;
+        
         expect(defaultLang).toEqual('1');
     })
 })
 
 describe("test logic components home", () => {
-    const wrapper = shallow(ShallowMock(<Home />, store));
+    const wrapper = mount(
+        <Provider store = {store}>
+            <Router>
+                <Home  />
+            </Router>
+        </Provider>
+    );
     
-    wrapper.find("#menu-film2").simulate("click");
-    it("button menu 2 click - update content", () => {
+    it("button category all click - update content", () => {
+        wrapper.find("#menu-film1").simulate("click");
+        const titleSectionContent = wrapper.find(".home-menu-item-text-active").text();
+        expect(titleSectionContent).toEqual('Semua');
+    })
+
+    it("button category now playing click - update content", () => {
+        wrapper.find("#menu-film2").simulate("click");
         const titleSectionContent = wrapper.find(".home-menu-item-text-active").text();
         expect(titleSectionContent).toEqual('Now Playing');
     })
 
-    wrapper.find("#menu-film3").simulate("click");
-    it("button menu 3 click - update content", () => {
+    it("button category popular click - update content", () => {
+        wrapper.find("#menu-film3").simulate("click");
         const titleSectionContent = wrapper.find(".home-menu-item-text-active").text();
         expect(titleSectionContent).toEqual('Popular');
     })
-
-    wrapper.find("#menu-film4").simulate("click");
-    it("button menu 4 click - update content", () => {
+    
+    it("button category top rated click - update content", () => {
+        wrapper.find("#menu-film4").simulate("click");
         const titleSectionContent = wrapper.find(".home-menu-item-text-active").text();
         expect(titleSectionContent).toEqual('Top Rated');
     })
 
-    wrapper.find("#menu-film5").simulate("click");
-    it("button menu 5 click - update content", () => {
+    it("button category upcoming click - update content", () => {
+        wrapper.find("#menu-film5").simulate("click");
         const titleSectionContent = wrapper.find(".home-menu-item-text-active").text();
         expect(titleSectionContent).toEqual('Upcoming');
     })
@@ -62,7 +79,7 @@ describe("test logic components home", () => {
 
 describe("snapshots component Home", () => {
     it("Home snapshots", () => {
-      const homeSnap = shallow(ShallowMock(<Home />, store));
+      const homeSnap = shallow(<Provider store={store}><Home /></Provider>);
       expect(toJson(homeSnap)).toMatchSnapshot()
     })
   
